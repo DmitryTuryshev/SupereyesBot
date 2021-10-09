2bb1e0090499869e9371a09f85678e276a41e2fe
Dmitry Turyshev <turyshev.dima@mail.ru>
2021-10-09 11:04:34 +1000

    Changed welcome message

--- app/handlers/common.py
+++ app/handlers/common.py
@@ -1,8 +1,7 @@
-
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
-from app.guard.check import check_access
+from app.guard.check import check_access, check, check_reg
 
 
 async def cmd_start(message: types.Message, state: FSMContext):
@@ -11,19 +10,23 @@ async def cmd_start(message: types.Message, state: FSMContext):
     if not check_access(message.from_user.id):
         await message.answer('У вас нет доступа')
         return
+    if not check_reg(message.from_user.id):
+        await message.answer('Вы не зарегистрированы.\n\n'
+                             'Зарегистрироваться (/reg)',  reply_markup=types.ReplyKeyboardRemove())
+        return
 
-    menu="Выберите, что хотите заказать:\n\n" \
-         "1. Зарегистрироваться (/reg)\n" \
-         "2. Книги (/book)\n" \
-         "3. Ближайший ДР (/birthday)\n" \
-         "4. Управление (/admin)\n"
+    menu = "Задайте вопрос боту:\n\n" \
+           "1. Книги (/book)\n" \
+           "2. Ближайший ДР (/birthday)\n"
+    if not check(message.from_user.id):
+        menu += "3. Управление (/admin)\n"
     await message.answer(
         menu,
         reply_markup=types.ReplyKeyboardRemove()
     )
 
 
-async def cmd_cancel(message:types.Message, state: FSMContext):
+async def cmd_cancel(message: types.Message, state: FSMContext):
     await state.finish()
     await message.answer("Действие отменено", reply_markup=types.ReplyKeyboardRemove())
 
--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher
 from aiogram.types.bot_command import BotCommand
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import configparser
@@ -8,35 +8,30 @@ from app.handlers.common import register_handlers_common
 from app.handlers.book import register_handlers_book
 from app.handlers.birthday import register_handlers_birthday
 from app.handlers.admin import register_handlers_admin
-from app.notification.notification import send_notification_to_admin, run_notification
+from app.notification.notification import run_notification
 import asyncio
-from time import sleep
 
+logger = logging.getLogger(__name__)
 
-logger=logging.getLogger(__name__)
 
 async def set_commands(bot: Bot):
-    commads=[
+    commads = [
         BotCommand(command="/reg", description="Зарегистрироваться"),
-        BotCommand(command="/book", description="Отменить текущее действие"),
-        BotCommand(command="/birthday", description="Отменить текущее действие"),
+        BotCommand(command="/book", description="Книги"),
+        BotCommand(command="/birthday", description="Ближайший ДР"),
         BotCommand(command="/cancel", description="Отменить текущее действие"),
     ]
     await bot.set_my_commands(commads)
 
 
-async def main():
+async def main(bot: Bot):
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     )
     logger.error("Starting bot")
 
-    config=configparser.ConfigParser()
-    config.read("config/bot.ini")
-
-    bot=Bot(token=config["tg_bot"]["token"])
-    dp=Dispatcher(bot, storage=MemoryStorage())
+    dp = Dispatcher(bot, storage=MemoryStorage())
 
     register_handlers_common(dp)
     register_handlers_user(dp)
@@ -50,16 +45,14 @@ async def main():
 
 
 async def cic():
-    taskA=loop.create_task(main())
     config = configparser.ConfigParser()
     config.read("config/bot.ini")
     bot = Bot(token=config["tg_bot"]["token"])
-    taskB=loop.create_task(run_notification(bot))
+    taskA = loop.create_task(main(bot))
+    taskB = loop.create_task(run_notification(bot))
     await asyncio.wait([taskA, taskB])
 
-if __name__=="__main__":
-    try:
-        loop=asyncio.get_event_loop()
-        loop.run_until_complete(cic())
-    except:
-        pass
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(cic())
